53157d31d7b5e4e78c0421586026c7117464806b
tincogs <tomoday91@gmail.com>
2020-10-21 12:03:42 -0500

    Host upcoming stays

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,28 +20,53 @@ class User < ApplicationRecord
       User.all.where(host: false)
     end
 
-    ## method shows all upcoming user trips 
-    def pending_trips
-      # if current_password     
-      if self.stays 
-      upcoming_stays = self.stays.select{|stay|stay.checkin > Date.today}
-      upcoming_stays_title = upcoming_stays.map{|stay|stay.property.title}
-      upcoming_stays_title 
-      else  
-      "No stays scheduled, click on the below link to plan stays."
-      end 
-    end 
+    ## method shows all upcoming user trips
+    def nomad_upcoming_stays
+     pending =  self.stays
+        .order(checkin: :desc)
+        .where.not(checkin: nil)
+        .select { |stay| stay.checkin > (Date.today) }
+     pending_titles = pending.map{|stay|stay.property.title}
+      if pending_titles.count > 0
+        pending_titles
+      else
+        "You have no scheduled stays. Click below to schedule one!"
+      end
+    end
 
-    # this is to show users previous trips 
-    def completed_trips 
-      if self.stays 
-        completed_stays = self.stays.select{|stay|stay.checkin < Date.today}
-        completed_stays_title = completed_stays.map{|stay|stay.property.title}
-        completed_stays_title
-      else 
+    # this is to show users previous trips
+    def nomad_completed_stays
+      completed =  self.stays
+         .order(checkout: :desc)
+         .where.not(checkout: nil)
+         .select { |stay| stay.checkout < (Date.today) }
+      completed_titles = completed.map{|stay|stay.property.title}
+        if completed_titles.count > 0
+          completed_titles
+        else
           "You have no past stays"
-      end 
-    end 
+        end
+    end
+
+    def host_stays
+      Stay.select{|stay| stay.property.host == self}
+    end
 
 
-  end 
+    def host_upcoming_stays
+      if self.host_stays.count > 0
+        pending =  self.host_stays
+         .order(checkin: :assc)
+         .where.not(checkin: nil)
+         .select { |stay| stay.checkin > (Date.today) }
+        pending_titles = pending.map{|stay|stay.property.title}
+        pending_titles
+      else
+        "You have no scheduled stays. Click below to schedule one!"
+      end
+    end
+
+
+
+
+  end
